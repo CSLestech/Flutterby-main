35476d63144ca2a8e7c9ced551de2e302b189b23
Laeeeee-csstudent <92740634+Laeeeee-csstudent@users.noreply.github.com>
2025-04-03 13:47:21 +0800

    NEW UPDATE

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -303,7 +303,8 @@ class HomeViewState extends State<HomeView> {
                     decoration: BoxDecoration(
                       boxShadow: [
                         BoxShadow(
-                          color: Colors.black.withOpacity(0.2),
+                          color: const Color.fromRGBO(
+                              0, 0, 0, 0.2), // Replace withOpacity
                           blurRadius: 10,
                           offset: const Offset(0, 5),
                         ),
