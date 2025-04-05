37bfe09eb58379d979c75627c2f405918c41d0e5
Laeeeee-csstudent <92740634+Laeeeee-csstudent@users.noreply.github.com>
2025-04-05 14:22:29 +0800

    NEW UPDATE ON onboardingscreen

--- lib/onboardingscreen.dart
+++ lib/onboardingscreen.dart
@@ -147,19 +147,3 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     );
   }
 }
-
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-      ),
-      body: const Center(
-        child: Text("Welcome to the Home Screen!"),
-      ),
-    );
-  }
-}
