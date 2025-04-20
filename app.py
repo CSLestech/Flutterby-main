e8969ce614f4064c0b290e3a525a8bdeb959501b
Laeeeee-csstudent <92740634+Laeeeee-csstudent@users.noreply.github.com>
2025-04-20 11:06:59 +0800

    new update

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ deit.to(device)
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5] * 3, std=[0.5] * 3)
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 # === 6. Flask App ===
