31eba8482e7c4cf83e0f9af23ffc3356b1d6e939
Laeeeee-csstudent <92740634+Laeeeee-csstudent@users.noreply.github.com>
2025-04-22 21:53:05 +0800

    new update

--- lib/history_page.dart
+++ lib/history_page.dart
@@ -1,3 +1,4 @@
+import 'package:check_a_doodle_doo/background_wrapper.dart';
 import 'package:check_a_doodle_doo/prediction_details_screen.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
@@ -19,102 +20,84 @@ class HistoryPage extends StatelessWidget {
         title: const Text("History"),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            onBackToHome(); // Navigate back to Home Page
-          },
+          onPressed: onBackToHome,
         ),
       ),
-      body: Stack(
-        children: [
-          // Background image
-          Positioned.fill(
-            child: Image.asset(
-              'images/ui/main_bg.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-
-          // Dark overlay
-          Container(
-            color: Colors.black.withAlpha(77),
-          ),
-
-          // Foreground content
-          history.isEmpty
-              ? const Center(
-                  child: Text(
-                    "No history available.",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontFamily: "Garamond",
-                      color: Colors.white,
-                    ),
+      body: BackgroundWrapper(
+        child: history.isEmpty
+            ? const Center(
+                child: Text(
+                  "No history available.",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: "Garamond",
+                    color: Colors.white,
                   ),
-                )
-              : ListView.builder(
-                  itemCount: history.length,
-                  itemBuilder: (context, index) {
-                    final entry = history[index];
-                    final String? imagePath = entry["imagePath"];
-                    final String timestamp = entry["timestamp"];
+                ),
+              )
+            : ListView.builder(
+                itemCount: history.length,
+                itemBuilder: (context, index) {
+                  final entry = history[index];
+                  final String? imagePath = entry["imagePath"];
+                  final String timestamp = entry["timestamp"];
 
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => PredictionDetailsScreen(
-                              imagePath: entry["imagePath"],
-                              prediction: entry["prediction"],
-                              timestamp: entry["timestamp"],
-                            ),
-                          ),
-                        );
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              if (imagePath != null &&
-                                  File(imagePath).existsSync())
-                                ClipRRect(
-                                  borderRadius: const BorderRadius.vertical(
-                                    top: Radius.circular(10),
-                                  ),
-                                  child: Image.file(
-                                    File(imagePath),
-                                    width: double.infinity,
-                                    height: 200,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  "Uploaded on: $timestamp",
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: "Garamond",
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ],
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PredictionDetailsScreen(
+                            imagePath: entry["imagePath"],
+                            prediction: entry["prediction"],
+                            timestamp: entry["timestamp"],
                           ),
                         ),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            if (imagePath != null &&
+                                File(imagePath).existsSync())
+                              ClipRRect(
+                                borderRadius: const BorderRadius.vertical(
+                                  top: Radius.circular(10),
+                                ),
+                                child: Image.file(
+                                  File(imagePath),
+                                  width: double.infinity,
+                                  height: 200,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Uploaded on: $timestamp",
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: "Garamond",
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    );
-                  },
-                ),
-        ],
+                    ),
+                  );
+                },
+              ),
       ),
     );
   }
