322a22b693912b27795d29b3d0b1cb0f38bd0794
Laeeeee-csstudent <92740634+Laeeeee-csstudent@users.noreply.github.com>
2025-05-06 22:44:54 +0800

    change uri

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -732,11 +732,11 @@ class HomeViewState extends State<HomeView>
 
     // List of possible server addresses to try in order
     final List<String> serverAddresses = [
-      "http://192.168.31.180:5000/predict", // Primary address
-      "http://172.30.48.1:5000/predict", // Alternative address 1
-      "http://172.31.80.1:5000/predict", // Alternative address 2
-      "http://10.0.2.2:5000/predict", // For Android emulator
-      "http://localhost:5000/predict" // For iOS simulator
+      "http://192.168.1.11:5000/predict", // Primary address
+      "http://192.168.31.180:5000/predict", // Alternative address 1
+      "http://172.30.48.1:5000/predict", // Alternative address 2
+      "http://172.31.80.1:5000/predict", // For Android emulator
+      "http://10.0.2.2:5000/predict" // For iOS simulator
     ];
 
     Exception? lastException;
